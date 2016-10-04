b9adc7a7b8e3621cb413c1c2f7f78ef37aae19ac
Alex Ferreira <alex@dsol.com.br>
2016-10-04 04:34:03 -0300

    add dependencies

--- mix.exs
+++ mix.exs
@@ -4,7 +4,8 @@ defmodule Metricx.Mixfile do
   def project do
     [app: :metricx,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.2",
+     elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -14,19 +15,24 @@ defmodule Metricx.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [ 
+      mod: {Metricx, []},
+      applications: [
+        :logger
+      ]
+    ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp elixirc_paths(:test), do: ["lib"]
+  defp elixirc_paths(_),     do: ["lib"]
+
   defp deps do
-    []
+    [
+      {:earmark, "~> 1.0", only: :docs},
+      {:ex_doc, "~> 0.14", only: :docs},
+      {:inch_ex, "~> 0.5", only: :docs},
+      {:mix_test_watch, "~> 0.2", only: :dev},
+      {:exactor, "~> 2.2"}
+    ]
   end
 end
