9aa7f3ff781e8ca3b0f18401f54f96db67a5699c
Alex Ferreira <alex@dsol.com.br>
2016-10-04 04:34:25 -0300

    add supervisor do counter

--- lib/metricx.ex
+++ lib/metricx.ex
@@ -1,2 +1,16 @@
 defmodule Metricx do
+  @moduledoc false
+
+  use Application
+
+  @doc false
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    children = [
+      worker(Metricx.Counter, [])
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
 end
